3b683a14adfc618a3d13680ba6c41d1c7af02156
Neo Jun Jie <neojunjie03@gmail.com>
2026-05-17 03:08:13 +0800

    removed loose exercise files

--- Exercise 1.py
+++
@@ -1,6 +0,0 @@
-name = "Alice"
-age = 25
-country = "Singapore"
-student = True
-
-print(f"Hi, I'm {name}, {age} years old, from {country}. Student: {student}")
--- Exercise 2.py
+++
@@ -1,4 +0,0 @@
-print(int(3.9))
-print(str(100) + "dollars")
-print(type(True))
-print(float("3.14") + 1)
