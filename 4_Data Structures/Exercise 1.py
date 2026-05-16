a090c936d60abf8b07840aee05bed8185fcb1e7a
Neo Jun Jie <neojunjie03@gmail.com>
2026-05-17 02:46:12 +0800

    Added data structures practices

---
+++ 4_Data Structures/Exercise 1.py
@@ -0,0 +1,7 @@
+shopping_list = ["apple", "bread", "toothpaste", "milk", "eggs"]
+
+print(shopping_list[0])
+print(shopping_list[-1])
+shopping_list.remove("bread")
+print(shopping_list)
+print(len(shopping_list))
---
+++ 4_Data Structures/Exercise 2.py
@@ -0,0 +1,13 @@
+student = {
+    "name" : "Poto",
+    "age" : 21,
+    "grade" : "A",
+    "subject" : "math"
+}
+
+print(student["name"])
+print(student["grade"])
+student["age"] = 20
+student["country"] = "Singapore"
+print(student.keys())
+print(student.values())
