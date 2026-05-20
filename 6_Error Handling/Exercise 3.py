cc140d4104354deaf30a1af94e1c1b359897bb9c
Neo Jun Jie <neojunjie03@gmail.com>
2026-05-20 21:43:18 +0800

    edited error handling exercise 3

--- 6_Error Handling/Exercise 3.py
+++ 6_Error Handling/Exercise 3.py
@@ -1,8 +1,10 @@
 def get_number():
-    try: 
-        a = int(input("Enter a number: "))
-        print(a)
-    except TypeError:
-        print("That's not a number, try again!")
+    while True:
+        try: 
+            number = int(input("Enter a number: "))
+            return number
+        except ValueError:
+            print("That's not a number, try again!")
 
-get_number()
+result = get_number()
+print(f"You entered: {result}")
